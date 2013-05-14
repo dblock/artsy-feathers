9d2e1a6a0e64173408e6429798b3744f88fa2ff9
dblock <dblock@dblock.org>
2013-05-14 12:38:11 -0400

    Fix: avoid duplicates by examining timeline and properly count towards 140 characters with truncation.

---
+++ examples/smart_truncate.rb
@@ -0,0 +1,8 @@
+# source: http://stackoverflow.com/questions/1293573/rails-smart-text-truncation
+def smart_truncate(text, char_limit)
+  size = 0
+  text.split().reject do |token|
+    size += (token.size() + 1)
+    size > char_limit
+  end.join(" ") + (text.size() > char_limit ? " ..." : "" )
+end
--- examples/tweet_new_shows.rb
+++ examples/tweet_new_shows.rb
@@ -1,20 +1,28 @@
 require 'artsy-client'
 require File.expand_path(File.join(File.dirname(__FILE__), 'twitter_auth.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), 'smart_truncate.rb'))
+
+# recently tweeted works or shows
+recent_urls = Twitter.user_timeline.map do |status|
+  status.urls.map(&:expanded_url)
+end.flatten
 
 Artsy::Client.authenticate!
 
 Artsy::Client.shows[:results].reverse.each do |show|
   puts "#{show}"
-  show.artworks.each do |artwork|
-    puts " #{artwork}"
-    next unless artwork.can_share_image
-    begin
-      show_info = [ show.partner, show.where, show.when ].compact.join(", ")
-      Twitter.update("#{artwork}\n#{show_info}\nhttp://artsy.net/artwork/#{artwork.id}")
-      break # one at a time
-    rescue Twitter::Error::Forbidden => e
-      puts "  ERROR: #{e.message}"
-    end
-    break # only one work
+  artwork = show.artworks.select { |a| a.can_share_image }.first
+  next unless artwork
+  puts " #{artwork}"
+  show_info = [ show.partner, show.where, show.when ].compact.join(", ")
+  url = "http://artsy.net/artwork/#{artwork.id}"
+  if recent_urls.include?(url)
+    puts "  Skipping, already tweeted."
+    next
   end
+  # links count 22 characters, see https://dev.twitter.com/docs/faq#5810
+  # + two CR/LFs
+  artwork_info = smart_truncate(artwork.to_s, 140 - 22 - 4 - show_info.length)
+  Twitter.update("#{artwork_info}\n#{show_info}\n#{url}")
+  break # one at a time
 end

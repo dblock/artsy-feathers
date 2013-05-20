493dcea4976251cfec32b1d567293369e6becf70
dblock <dblock@dblock.org>
2013-05-20 16:30:48 -0400

    The certificate issues on https://artsyapi.com have been fixed.

--- examples/artsy_config.rb
+++ examples/artsy_config.rb
@@ -2,6 +2,5 @@ require 'artsy-client'
 
 Artsy::Client.configure do |config|
   config.logger = Logger.new(STDOUT)
-  config.logger.level = Logger::DEBUG
-  config.endpoint = "http://artsyapi.com"
+  config.logger.level = Logger::INFO
 end

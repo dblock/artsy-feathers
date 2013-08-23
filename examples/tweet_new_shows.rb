54b28d4218819a3e111254418896d0f80540c74e
dblock <dblock@dblock.org>
2013-08-23 18:03:00 -0400

    Backward compatibility for artworks.

--- examples/tweet_new_shows.rb
+++ examples/tweet_new_shows.rb
@@ -10,16 +10,27 @@ end.flatten
 Artsy::Client.authenticate!
 
 Artsy::Client.shows[:results].reverse.each do |show|
+
   puts "#{show}"
+
+  artwork = show.artworks.detect { |a| a.can_share_image }
+  url = "http://artsy.net/artwork/#{artwork.id}"
+  if recent_urls.include?(url)
+    puts "  Skipping, already tweeted."
+    next
+  end
+
   show_info = [ show.partner, show.where, show.when ].compact.join(", ")
   url = "http://artsy.net/show/#{show.id}"
   if recent_urls.include?(url)
     puts "  Skipping, already tweeted."
     next
   end
+
   # links count 22 characters, see https://dev.twitter.com/docs/faq#5810
   # + two CR/LFs
   show_info = smart_truncate(show_info.to_s, 140 - 22 - 3)
   Twitter.update("#{show_info}\n#{url}")
   break # one at a time
+
 end
